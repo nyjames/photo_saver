aefd6f05ce6f017f5e29134b8c9c8ac45503aafd
Nya James <130016910+nyjames@users.noreply.github.com>
2025-03-17 16:09:08 -0400

    Initial commit

---
+++ Photo_Saver/Photo_SaverApp.swift
@@ -0,0 +1,17 @@
+//
+//  Photo_SaverApp.swift
+//  Photo_Saver
+//
+//  Created by Nya James on 3/17/25.
+//
+
+import SwiftUI
+
+@main
+struct Photo_SaverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Photo_SaverTests/Photo_SaverTests.swift
@@ -0,0 +1,17 @@
+//
+//  Photo_SaverTests.swift
+//  Photo_SaverTests
+//
+//  Created by Nya James on 3/17/25.
+//
+
+import Testing
+@testable import Photo_Saver
+
+struct Photo_SaverTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
